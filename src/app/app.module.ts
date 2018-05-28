b1f7e5176b40c8da8d49458e7c4480198af63302
tharak1991 <tharak1991@gmail.com>
2018-05-29 00:26:05 +0530

    packages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -12,6 +12,7 @@ import {
   MatDialog, MatDialogRef, MAT_DIALOG_DATA
 } from '@angular/material';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {WebStorageModule, LocalStorageService} from 'angular-localstorage';
 
 
 import { AppComponent } from './app.component';
